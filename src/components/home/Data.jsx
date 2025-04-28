e0e7a26cd7d4ede65daa7e8afe4bb2f05f1ebbd6
Adina Martiniuc <adinamartiniuc@Adinas-MacBook-Air.local>
2025-04-28 16:46:35 +0300

    Add GTM integration for Google Analytics

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Data = () => {
+    const [abGroup, setAbGroup] = useState('A');
+
+    useEffect(() => {
+        const group = localStorage.getItem('abGroup') || 'A';
+        setAbGroup(group);
+      }, []);
+
     return(
         <div className="home__data">
-            <h1 className="home__title">Adina</h1>
+            <section className="hero">
+            <h1>
+                {abGroup === 'A'
+                ? "Hi, I'm Adina -- Front-End Developer"
+            : "Hey there! I design & code clean interfaces"}
+            </h1>
+
+            </section>
 
             <h3 className="home__subtitle">Software Developer</h3>
             <p className="home__description">I'm a software developer based in Suceava, always eager to expand my knowledge and grow both personally and professionally. 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GTM from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: "GTM-TRHSCWD8", // replace with your GTM ID
+};
+
+GTM.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
